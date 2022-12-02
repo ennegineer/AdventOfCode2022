7d5a5e3e0b4116f5960514d7f545c47f3c7dcfe2
ennegineer <enne.face@gmail.com>
2022-12-01 19:43:56 -0800

    Day 1 Part 1 complete!

---
+++ Day1/main.py
@@ -0,0 +1,27 @@
+# Calorie Counting
+
+# PART ONE:
+# Count the number of calories each elf is carrying.
+# Report the total carried by the elf carrying the most calories.
+
+# Import the data
+with open("input.txt", "r") as input:
+    data = input.read().split('\n')
+
+def PartOne():
+    # Sum each group. A group ends when a blank line is reached.
+    counter = 0
+    elves = []
+
+    for row in data:
+        if row != '':
+            counter += int(row)
+        elif row == '':
+            elves.append(counter)
+            counter = 0
+
+    # Find the max in the group.
+    maximum = max(elves)
+    print(maximum)
+
+PartOne()
